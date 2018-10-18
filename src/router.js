f7885b5026df600dea8c861d586c637e1713b690
Onlysuper <2483613372@qq.com>
2018-10-18 13:14:35 +0800

    登录页面制作

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Layout from '@/views/Layout'
+import Layout from '@/views/LayoutPage'
+import Login from '@/views/Login'
 Vue.use(Router)
 
 export default new Router({
@@ -9,11 +10,24 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/login',
+      component: Login,
+    },
+    {
       path: '',
       component: Layout,
       // redirect: "dashboard",
       children: [
         { path: 'dashboard', component: () => import('@/views/Dashboard/index.vue'), name: 'dashboard', meta: { title: '首页', keepAlive: true } },
+      ]
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: Layout,
+      redirect: "./user/user",
+      children: [
+        { path: 'user', component: () => import('@/views/User/user'), name: 'user', meta: { title: '管理员', keepAlive: true } },
       ]
     },
     {
